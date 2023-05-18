e9885ae7945bbd56ee84f130baf3c2a87a652d1c
owoguva <ramish.xasanov@bk.ru>
2023-05-18 21:56:10 +0600

    hw1

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -22,11 +22,11 @@ from product import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/products/', views.product_list),
-    path('api/products/<int:id>/', views.product_detail),
-    path('api/category/', views.category_list),
-    path('api/category/<int:id>/', views.category_detail),
-    path('api/review/', views.review_list),
-    path('api/review/<int:id>/', views.review_detail),
+    path('api/v1/products/', views.product_list),
+    path('api/v1/products/<int:id>/', views.product_detail),
+    path('api/v1/category/', views.category_list),
+    path('api/v1/category/<int:id>/', views.category_detail),
+    path('api/v1/review/', views.review_list),
+    path('api/v1/review/<int:id>/', views.review_detail),
 
 ]
